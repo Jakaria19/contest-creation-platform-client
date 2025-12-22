31769d2677d7f80c0d9fc0b146d5ede63d405291
Jakaria <jakaria20031219@gmail.com>
2025-12-22 21:48:48 +0600

    banner added

---
+++ src/pages/Home/Banner.jsx
@@ -0,0 +1,28 @@
+const Banner = () => {
+  return (
+    <div className="p-5 md:px-10">
+      <h1 className="text-4xl font-extrabold text-center mt-10 mb-14">
+        Online Contests & Competitions
+      </h1>
+      <div className="flex flex-col md:flex-row items-center gap-6 my-9">
+        <div className="flex-1 p-5 ">
+          <h3 className="text-3xl font-extrabold">
+            <span className="text-[#1bc5bd]">
+              Grow and reward your communities with online
+            </span>{" "}
+            competitions. Drive engagement, increase subscribers, build loyalty.
+          </h3>
+        </div>
+        <div className="flex-1 p-5">
+          <img
+            className="rounded-lg"
+            src="https://static.drimify.com/wp-content/uploads/2023/02/online-contests-1-800x500.jpg?220823"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import AllContestHome from "./AllContestHome";
+import Banner from "./Banner";
+import Solution from "./Solution";
+
+const Home = () => {
+  return (
+    <div>
+      <Banner></Banner>
+      <AllContestHome></AllContestHome>
+      <Solution></Solution>
+    </div>
+  );
+};
+
+export default Home;

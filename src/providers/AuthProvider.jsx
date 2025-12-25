03cf0cc0ecce82e3354a7e232c5e804aa46f94cb
Jakaria <jakaria20031219@gmail.com>
2025-12-25 14:28:13 +0600

    vercel

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,34 +1,61 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import {
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import { app } from "../firebase/firebase.config";
 
 export const AuthContext = createContext(null);
+const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState({
-    displayName: "Test User",
-    email: "test@example.com",
-    photoURL: "https://i.ibb.co/p3d9pYn/user.png",
-  });
-
-  const [loading, setLoading] = useState(false);
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const googleProvider = new GoogleAuthProvider();
 
   const createUser = (email, password) => {
-    return Promise.resolve();
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
   };
+
   const signIn = (email, password) => {
-    return Promise.resolve();
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
   };
+
   const googleSignIn = () => {
-    return Promise.resolve();
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
   };
 
   const logOut = () => {
-    setUser(null);
-    return Promise.resolve();
+    setLoading(true);
+    return signOut(auth);
   };
 
   const updateUserProfile = (name, photo) => {
-    return Promise.resolve();
+    return updateProfile(auth.currentUser, {
+      displayName: name,
+      photoURL: photo,
+    });
   };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      console.log("Current User Status:", currentUser);
+      setLoading(false);
+    });
+    return () => {
+      return unsubscribe();
+    };
+  }, []);
 
   const authInfo = {
     user,
